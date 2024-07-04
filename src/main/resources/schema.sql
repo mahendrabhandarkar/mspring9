6549c48dc56c0f2b10551803797ed8c9c3d5a5b8
Mahendra Bhandarkar <mahendrabhandarkar@gmail.com>
2024-07-04 10:05:57 +0530

    Create schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+create table t_user (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(256), email VARCHAR(256), password VARCHAR(256));
+insert into t_user (username, email, password) VALUES ('admin2','test2@test.com','admin2');
